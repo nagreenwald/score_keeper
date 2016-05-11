18daae573e1a2be49664a687f0073415d7ff9727
Nick Greenwald <greenwaldnick@gmail.com>
2016-05-10 18:41:08 -0700

    second

---
+++ app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+var score=0;
+
+
+    $(document).on("click", "#increase-5", function() {
+
+      score=score+5;
+      $("#score").html(score + " Points");
+
+
+
+
+});
+
+$(document).on("click", "#decrease-5", function() {
+
+  score=score-5;
+  $("#score").html(score + " Points");
+
+
+
+
+});
+
+$(document).on("click", "#submit-custom-score", function() {
+score = parseInt($("#custom-score").val());
+$("#score").html(score + " Points");
+
+});
+});
